7d3ef694d7579fbff2ad2dcc96b7019e32dc728b
Miklva <147387609+Miklva@users.noreply.github.com>
2026-02-25 18:18:03 +0100

    Modifs de style

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,11 +1,14 @@
 import { Link, Outlet } from "react-router-dom";
 import "./Layout.scss";
+import logo from "../assets/logo.png";
 
 export default function Layout() {
     return (
         <div className="layout">
             <nav className="navbar">
-                <Link to="/" >SupContent</Link>
+                <Link to="/">
+                    <img src={logo} alt="Logo SupContent" className="logo" />
+                </Link>
                 <div className="links">
                     <Link to="/profile">Profil</Link>
                     <Link to="/settings">Paramètre</Link>
