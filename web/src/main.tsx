13cf3b57b622fdbb2f938d5219e95cc3728e789d
XxXGri <antoine.renaudin@supinfo.com>
2026-06-14 16:24:57 +0200

    Mobile debug

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import axios from 'axios'
+import { Capacitor } from '@capacitor/core'
+
+if ( Capacitor.isNativePlatform() )
+{
+  axios.defaults.baseURL = 'http://10.0.2.2:5000'
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
